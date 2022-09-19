9e1c76e81459eea909d2e91f1768220bd15e03f2
MELTUser <MELTUser@MELT-GP-S518N>
2022-09-19 16:56:18 +1000

    Validation logic demo

---
+++ Validation_Demo/Name.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation_Demo
+{
+    class Name:GenericString
+    {
+        public Name(string nameValue) : base(nameValue)
+        {
+            InvalidMessage = "The name must be non-blank";
+        }
+
+        public override bool ValidateString(string input)
+        {
+            return input.Length > 0;
+        }
+    }
+}
---
+++ Validation_Demo/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Validation_Demo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            GetValidatedString("name");
+            GetValidatedString("email");
+        }
+        
+        public static GenericString GetValidatedString(string type)
+        {
+            GenericString validatedString = null;
+            Console.WriteLine("Please enter the name");
+            string input = Console.ReadLine();
+            // Validation using polymorphism
+            if (type == "name")
+            {
+                validatedString = new Name(input);
+            } else if (type == "email")
+            {
+                validatedString = new Email(input);
+            }
+            
+            while (!validatedString.IsValid)
+            {
+                Console.WriteLine($"The client's name is invalid, please reset it");
+                input = Console.ReadLine();
+                validatedString.Value = input;
+            }
+            Console.WriteLine($"The client's name is {validatedString.Value}");
+            return validatedString;
+        }
+
+        public static Name GetClientName()
+        {
+            Console.WriteLine("Please enter the name");
+            string input = Console.ReadLine();
+            Name clientName = new Name(input);
+            while (!clientName.IsValid)
+            {
+                Console.WriteLine($"The client's name is invalid, please reset it");
+                input = Console.ReadLine();
+                clientName.Value = input;
+            }
+            Console.WriteLine($"The client's name is {clientName.Value}");
+            return clientName;
+        }
+
+        public static Email GetClientEmail()
+        {
+            Console.WriteLine("Please enter the email");
+            string input = Console.ReadLine();
+            Email clientEmail = new Email(input);
+            while (!clientEmail.IsValid)
+            {
+                Console.WriteLine($"The client's name is invalid, please reset it");
+                input = Console.ReadLine();
+                clientEmail.Value = input;
+            }
+            Console.WriteLine($"The client's name is {clientEmail.Value}");
+            return clientEmail;
+        }
+    }
+}
